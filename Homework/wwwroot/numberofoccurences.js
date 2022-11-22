6b625e7bcffee6acaaee60eb1af3aa17bdec9ff5
Andrey Chasovskoy <andrchas777@gmail.com>
2022-11-22 22:21:21 +0200

    AddedFiles

---
+++ Homework/wwwroot/numberofoccurences.js
@@ -0,0 +1,21 @@
+﻿function numberOfOccurences() {
+    var userIn = prompt("Enter some text", "");
+    var dictionary = {};
+    var arr = [];
+
+    for (var index in userIn) {
+        arr.push(userIn[index]);
+    }
+
+    for (var i = 0; i < arr.length; i++) {
+        if (isNaN(dictionary[arr[i]])) {
+            dictionary[arr[i]] = 1;
+        }
+        else {
+            dictionary[arr[i]] = dictionary[arr[i]] + 1;
+        }
+    }
+
+    var text = JSON.stringify(dictionary);
+    document.getElementById("result").innerHTML = text;
+}
